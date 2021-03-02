2f4528171b8aa9e11cf35606056a26ed96781e47
kato-masaki <0913ritz@gmail.com>
2021-03-02 13:42:53 +0000

    [Fix] usersコントローラーのbefore_action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :ensure_correct_user, only: [:update]
+  before_action :ensure_correct_user, only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -18,12 +18,12 @@ class UsersController < ApplicationController
 
   def update
     if @user.update(user_params)
-      redirect_to users_path(@user), notice: "You have updated user successfully."
+      redirect_to user_path(@user), notice: "You have updated user successfully."
     else
-      render "show"
+      render "edit"
     end
   end
-
+  
   private
   def user_params
     params.require(:user).permit(:name, :introduction, :profile_image)
@@ -34,5 +34,5 @@ class UsersController < ApplicationController
     unless @user == current_user
       redirect_to user_path(current_user)
     end
-  end 
+  end
 end
